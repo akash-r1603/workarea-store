0e7a0e96cae974db368337f43e19811b6a7f9f12
rails-10 <akash@corewebconnections.com>
2022-12-13 17:18:56 +0530

    Exercise 2: Create a new view partial - create a new partial sale_widget - append this to to product details page below price - added a check for sale

---
+++ config/initializers/appends.rb
@@ -0,0 +1,16 @@
+Workarea.append_partials(
+  'admin.variant_fields',
+  'workarea/admin/catalog_varients/new_arrival' 
+)
+
+
+Workarea.append_partials(
+  'storefront.product_details',
+  'workarea/storefront/products/new_arrival'
+)
+
+
+Workarea.append_partials(
+  'storefront.product_pricing_details',
+  'workarea/storefront/products/sale_widget'
+)
